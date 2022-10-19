ad48a77ebc14af151aa3a154aefd82fa97136916
Alexander Fedorov <alexander_fedorov1971@mail.ru>
2022-10-19 21:14:42 +0300

    port to mysql

--- src/VirtoCommerce.AssetsModule.Data/Repositories/DesignTimeDbContextFactory.cs
+++ src/VirtoCommerce.AssetsModule.Data/Repositories/DesignTimeDbContextFactory.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using System;
 
 namespace VirtoCommerce.AssetsModule.Data.Repositories
 {
@@ -9,7 +10,9 @@ namespace VirtoCommerce.AssetsModule.Data.Repositories
         {
             var builder = new DbContextOptionsBuilder<AssetsDbContext>();
 
-            builder.UseSqlServer("Data Source=(local);Initial Catalog=VirtoCommerce3;Persist Security Info=True;User ID=virto;Password=virto;MultipleActiveResultSets=True;Connect Timeout=30");
+            //builder.UseSqlServer("Data Source=(local);Initial Catalog=VirtoCommerce3;Persist Security Info=True;User ID=virto;Password=virto;MultipleActiveResultSets=True;Connect Timeout=30");
+
+            builder.UseMySql("server=localhost;user=root;password=virto;database=VirtoCommerce3;", new MySqlServerVersion(new Version(5, 7)));
 
             return new AssetsDbContext(builder.Options);
         }
